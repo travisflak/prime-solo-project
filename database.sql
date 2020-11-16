4ca35dd88a0bec50c9df1a62a11a9817a15cb893
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-16 14:12:57 -0600

    created goceries table in postico and updated the database sql file with the table.

--- database.sql
+++ database.sql
@@ -9,3 +9,12 @@ CREATE TABLE "user" (
     "password" VARCHAR (1000) NOT NULL,
     "admin" BOOLEAN default false
 );
+
+-- add groceries table
+CREATE TABLE "groceries" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" int NOT NULL,
+    "item" VARCHAR (1000) NOT NULL,
+    "quantity" int,
+    "shopped" BOOLEAN default false
+);
