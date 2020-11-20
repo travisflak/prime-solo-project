5f578eeae519b7abb821664375b626359bfee398
Travis Flak <travisflak@traviss-MacBook-Air.local>
2020-11-20 13:11:50 -0600

    successfully getting the quantity data and automatically putting that data in the input field that's rendered to the DOM.

--- src/components/groceryList/groceryList.js
+++ src/components/groceryList/groceryList.js
@@ -4,6 +4,14 @@ import mapStoreToProps from '../../redux/mapStoreToProps';
 
 class groceryList extends Component {
 
+    state = {
+        newItem: {  
+            item: '',
+            quantity: 0,
+            shopped: false,
+        }
+    };
+
     componentDidMount() {
     this.getGroceries();
     }
@@ -14,27 +22,24 @@ class groceryList extends Component {
         this.props.dispatch({type: 'FETCH_GROCERIES'});
     }
 
-  state = {
-    heading: 'Grocery List',
-  };
-
   render() {
     return (
       <div>
         <h2>{this.state.heading}</h2>
-        <pre>{JSON.stringify(this.props.reduxState)}</pre>
+        {/* <pre>{JSON.stringify(this.props.store.groceryListItems)}</pre> */}
         <header>
             <button onClick={() => this.props.history.push('/addItem')}>Add Item to List</button>
             <button>Refresh Shopped Items</button>
         </header>
-        <input type="number" onChange={(event) => this.setState({groceryList:event.target.value})}/>
+        {/* <input type="number" onChange={(event) => this.setState({groceryList:event.target.value})}/> */}
         
+        {/* {JSON.stringify(this.state.newItem.quantity)} */}
         
         <ol>
             {this.props.store.groceryListItems.map((groceryItem) => {
                 return(
-                <li>{groceryItem.item}{groceryItem.quantity}
-                    
+                <li>{groceryItem.item}
+                    <input type="number" defaultValue={groceryItem.quantity} onChange={(event) => this.setState({groceryList:event.target.value})}/>
                     <button>Shopped?</button>
                     <button>Delete Item</button>
                 </li>)
